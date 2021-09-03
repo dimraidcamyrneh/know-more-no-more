ae70c837db3a61f0c6ff138b13e3ef9f93eed5d8
dimraidcamyrneh <contact@henrymacdiarmid.com>
2021-09-03 13:00:24 -0700

    Finished Gatsby Tutorial Part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        alt="Steven Parrino, Untitled, 1998, Enamel and graphite pencil on paper, 17 1/8 × 18 7/8in"
+        src="../images/Parrino_Steven-1998-Untitled.jpeg"
       />
     </Layout>
   )
